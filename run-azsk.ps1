c7e21d13044c7523bb39a91ffda10d2f254bccba
Puneet Ghanshani <punit.ganshani@gmail.com>
2020-12-03 09:37:05 +0800

    Delete run-azsk.ps1

--- run-azsk.ps1
+++
@@ -1,7 +0,0 @@
-Install-Module AzSK -AllowClobber -Force -SkipPublisherCheck
-
-Set-AzSKPolicySettings -AutoUpdate On
-
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/punitganshani/azsk-poc/main/azuredeploy.json?token=AAH4C2LYPSV4QMFVMDR7YT27Y4XUE -OutFile azuredeploy.json
-
-Get-AzSKARMTemplateSecurityStatus -ARMTemplatePath .\azuredeploy.json
